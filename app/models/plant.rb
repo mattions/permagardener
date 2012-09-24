acc5c385d3b6baa2380c43ff08397562dd59f045
Michele Mattioni <mattions@gmail.com>
2012-09-24 14:50:36 +0100

    Added validation for the forms, and links to all the plants.

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,8 @@
 class Plant < ActiveRecord::Base
   attr_accessible :blooming, :description, :harvesting, :name, :sowing
+  
+  validates :name,  :presence => true
+  validates :description, :presence => true
+  
+  
 end
