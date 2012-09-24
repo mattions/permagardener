288870f26ff3ce01a29a2ce745f127f0f16fb25a
Michele Mattioni <mattions@gmail.com>
2012-09-24 14:32:17 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Permagardener::Application.config.secret_token = '1f790caf97ab5a26f2e59e8e8352c3ab673b68047b478f36d790d00c1ebbfc3cd53497ae5b0850b4e8386fc463c84ea79b0e8baa87827b8ac6f637245bb9dbfb'
