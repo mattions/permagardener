9316edc8539d000ae10fa97f7ab8425057777fc5
Michele Mattioni <mattions@gmail.com>
2012-10-03 19:59:53 +0100

    added the create action and the partial to display the comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,7 @@
 class CommentsController < ApplicationController
+  def create
+    @plant = Plant.find(params[:plant_id])
+    @comment = @plant.comments.create(params[:comment])
+    redirect_to plant_path(@plant)
+  end
 end
